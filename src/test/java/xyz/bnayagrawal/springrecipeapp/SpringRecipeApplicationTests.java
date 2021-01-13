e24eb840d50e8fe1d3da950613593212a5a6a198
bnayagrawal <bnayagrawal@gmail.com>
2021-01-13 11:50:27 +0530

    Add project files.

---
+++ src/test/java/xyz/bnayagrawal/springrecipeapp/SpringRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.bnayagrawal.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
